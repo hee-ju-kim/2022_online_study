b319d7412482d1d3415b202251aed0ca5477dbbc
heeju <bbbb412@naver.com>
2022-09-06 21:51:09 +0900

    20220906_2150

---
+++ AW/JS/20220926.js
@@ -0,0 +1,17 @@
+// 1. 변수생성
+/* 지시사항 1번 내용을 코드로 작성해보세요. */
+var fruit;
+fruit = "apple";
+
+/* 지시사항 2번 내용을 코드로 작성해보세요. */
+var box = "banana";
+
+/* 지시사항 3번 내용을 코드로 작성해보세요. */
+document.writeln(fruit);
+document.writeln(box);
+
+/* 지시사항 4번 내용을 코드로 작성해보세요. */
+box = "tomato";
+
+/* 지시사항 5번 내용을 코드로 작성해보세요. */
+document.writeln(box);
